98856b59f81c9685de924cccac702b7f720f1f3c
klanpakwaenchananyoo <klanpakwaenchananyoo@gmail.com>
2026-03-10 20:17:17 +0700

    lab24_3.cpp

--- lab24_3.cpp
+++ lab24_3.cpp
@@ -1,14 +1,14 @@
-#include<iostream>
+#include <set> // อย่าลืม include set เข้ามาด้วยนะครับ
 
-using namespace std;
-
-//Write function count() here.
-
-int main(){	
-	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
-	
-	cout << "There are " << count(data,sizeof(data)/sizeof(int)); 
-	cout << " different numbers in data.";
-	
-	return 0;
+int count(int data[], int size) {
+    // 1. สร้าง set ของเลขจำนวนเต็มขึ้นมา
+    set<int> unique_numbers;
+    
+    // 2. วนลูปเพื่อนำข้อมูลจาก array เข้าไปใน set
+    for(int i = 0; i < size; i++) {
+        unique_numbers.insert(data[i]);
+    }
+    
+    // 3. คืนค่าขนาด (size) ของ set ซึ่งก็คือจำนวนสมาชิกที่ไม่ซ้ำกันนั่นเอง
+    return unique_numbers.size();
 }
