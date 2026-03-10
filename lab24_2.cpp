3ad6d5833765c8c4fec967350998c88470751b33
klanpakwaenchananyoo <klanpakwaenchananyoo@gmail.com>
2026-03-10 20:16:40 +0700

    lab24_2.cpp

--- lab24_2.cpp
+++ lab24_2.cpp
@@ -24,7 +24,39 @@ int main(){
 	line_up.insert(loc,"Oscar");
 	
 	//Write your code here
-	
+	// Luffy, Sanji, Nami มาต่อแถวด้านหลังตามลำดับ
+	line_up.push_back("Luffy");
+	line_up.push_back("Sanji");
+	line_up.push_back("Nami");
+
+	// 2 คนที่หัวแถวได้เข้าไปในส้วมแล้ว (ลบ Alice และ Oscar)
+	line_up.pop_front();
+	line_up.pop_front();
+
+	// Narutu มาแทรกแถวหลัง Luffy
+	loc = find(line_up.begin(), line_up.end(), "Luffy");
+	if (loc != line_up.end()) {
+		advance(loc, 1); // ขยับไป 1 ตำแหน่งเพื่อให้แทรก "หลัง" Luffy
+		line_up.insert(loc, "Narutu");
+	}
+
+	// Prayath ท้องเสียปวดขี้สุด ๆ เลยมาแทรกแซวหน้าสุด
+	line_up.push_front("Prayath");
+
+	// Tony ยอมไม่ได้เลยมาแทรกด้านหลัง Prayath
+	loc = find(line_up.begin(), line_up.end(), "Prayath");
+	if (loc != line_up.end()) {
+		advance(loc, 1);
+		line_up.insert(loc, "Tony");
+	}
+
+	// Bob ทนไม่ไหวเลย เลยเปลี่ยนใจออกไปขี้ที่อื่น
+	line_up.remove("Bob");
+
+	// 3 คนที่หัวแถวได้เข้าไปในส้วมแล้ว (ลบ Prayath, Tony และ Luffy)
+	line_up.pop_front();
+	line_up.pop_front();
+	line_up.pop_front();
 	printList(line_up);
 		
 	return 0;
